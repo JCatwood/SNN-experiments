07dbb6d463c87b8c95b76e0ceb7536d1894d56e5
Jian Cao <jcao2416@gmail.com>
2025-02-10 15:48:59 -0600

    File rename

--- lowdim_SNN.R
+++ lowdim_SNN.R
@@ -10,16 +10,8 @@ library(scoringRules)
 # input args ---------------------------------------
 rm(list = ls())
 set.seed(123)
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) > 0) {
-  reorder_mtd <- as.numeric(args[1])
-} else {
-  # reorder_mtd
-  #  0 for no reordering
-  #  1 for maximin
-  #  2 for Vecchia as in VeccTMVN
-  reorder_mtd <- 0
-}
+# reorder_mtd
+reorder_mtd <- 0
 # sim setup ---------------------------------------
 tmp_vec <- seq(from = 0.025, to = 0.975, by = 0.05)
 locs <- as.matrix(expand.grid(tmp_vec, tmp_vec))
@@ -28,7 +20,7 @@ n <- nrow(locs)
 m <- 30
 range_parm <- 0.1
 nu_parm <- 1.5
-levl_censor <- rnorm(n)
+levl_censor <- rep(1, n)
 covmat <- fields::Matern(as.matrix(dist(locs)),
   range = range_parm,
   nu = nu_parm
@@ -39,54 +31,18 @@ mask_cens <- y < levl_censor
 n_cens <- sum(mask_cens)
 y_obs <- y
 y_obs[mask_cens] <- NA # levl_censor[mask_cens]
-NN <- nn2(locs, k = m + 1)[[1]]
 n_samp <- 50
-all(NN[, 1] == 1:n)
-order_new <- NULL
-if (reorder_mtd == 1) { # maximin
-  order_new <- GpGp::order_maxmin(locs)
-  locs_order <- locs[order_new, , drop = FALSE]
-  covmat <- fields::Matern(as.matrix(dist(locs_order)),
-    range = range_parm,
-    nu = nu_parm
-  )
-  y_obs <- y_obs[order_new]
-  mask_cens <- mask_cens[order_new]
-  NN <- nn2(locs_order, k = m + 1)[[1]]
-  levl_censor <- levl_censor[order_new]
-} else if (reorder_mtd == 2) { # Vecc
-  order_new <- VeccTMVN::Vecc_reorder(rep(-Inf, n), y_obs, m, covMat = covmat)$order
-  locs_order <- locs[order_new, , drop = FALSE]
-  covmat <- fields::Matern(as.matrix(dist(locs_order)),
-    range = range_parm,
-    nu = nu_parm
-  )
-  y_obs <- y_obs[order_new]
-  mask_cens <- mask_cens[order_new]
-  NN <- nn2(locs_order, k = m + 1)[[1]]
-  levl_censor <- levl_censor[order_new]
-}
 # generate TMVN samples using multi-threads -------------------
 ncores <- 4
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
 y_samp_SNN <- foreach(i = 1:n_samp, .packages = c("nntmvn")) %dopar% {
-  rtmvn_snn(y_obs, rep(-Inf, n), levl_censor, mask_cens,
-    m = m,
-    covmat = covmat, ordering = 1, seed = i
+  rptmvn(y_obs, rep(-Inf, n), levl_censor, mask_cens,
+    m = m, locs = locs,
+    covmat = covmat, ordering = reorder_mtd, seed = i
   )
 }
 stopCluster(cl)
-if (!is.null(order_new)) {
-  order_new_rev <- 1:n
-  order_new_rev[order_new] <- 1:n
-  y_samp_SNN <- lapply(y_samp_SNN, function(x) {
-    x[order_new_rev]
-  })
-  mask_cens <- mask_cens[order_new_rev]
-  NN <- nn2(locs, k = m + 1)[[1]]
-  levl_censor <- levl_censor[order_new_rev]
-}
 y_samp_SNN <- matrix(unlist(y_samp_SNN), n, n_samp, byrow = FALSE)
 y_cens_samp_SNN <- y_samp_SNN[mask_cens, ]
 
@@ -138,25 +94,6 @@ qqplot(as.vector(y_cens_samp_MET), as.vector(y_cens_samp_SNN),
 )
 abline(a = 0, b = 1, col = "red", lwd = 2)
 dev.off()
-
-# compute log-score for the samples -----------------------------
-mean(mvtnorm::dmvnorm(t(y_cens_samp_SNN),
-  mean = cond_mean_cens,
-  sigma = cond_covmat_cens, log = TRUE
-))
-sd(mvtnorm::dmvnorm(t(y_cens_samp_SNN),
-  mean = cond_mean_cens,
-  sigma = cond_covmat_cens, log = TRUE
-)) / sqrt(n_samp)
-mean(mvtnorm::dmvnorm(t(y_cens_samp_MET),
-  mean = cond_mean_cens,
-  sigma = cond_covmat_cens, log = TRUE
-))
-sd(mvtnorm::dmvnorm(t(y_cens_samp_MET),
-  mean = cond_mean_cens,
-  sigma = cond_covmat_cens, log = TRUE
-)) / sqrt(n_samp)
-cat("Log-score may not make sense as we are not estimating a model\n")
 
 # compute RMSE ------------------------------------
 cat(
