cf30dfb712c9c4edab6f8161adf9d19fb4410832
JCatwood <jcao2416@gmailc.om>
2025-07-22 22:46:42 -0500

    Add results of m_cmp with SNN_unknown_maximin. Updated plot style to score v.s. time

--- section_4/performance_plot.R
+++ section_4/performance_plot.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(scales)
+library(dplyr)
+library(tidyr)
 
 m_cmp_rslt <- read.table("m_cmp.csv", header = FALSE, sep = ",")
 mtd_cmp_rslt <- read.table("mtd_cmp.csv", header = FALSE, sep = ",")
@@ -8,6 +10,7 @@ colnames(mtd_cmp_rslt) <- c("scenario", "score", "method", "cov_kernel", "value"
 mtd_cmp_rslt$m <- NA
 mtd_cmp_rslt$m[mtd_cmp_rslt$method == "SNN"] <- 30
 mtd_cmp_rslt$m[mtd_cmp_rslt$method == "SNN_order_maximin"] <- 30
+mtd_cmp_rslt$m[mtd_cmp_rslt$method == "VT"] <- 30
 mtd_cmp_rslt <- mtd_cmp_rslt[c(1, 6, 2, 3, 4, 5)]
 all_rslt <- rbind(m_cmp_rslt, mtd_cmp_rslt)
 all_rslt <- all_rslt[!all_rslt$method == "SNN_order_ascd", ] # remove order_ascd
@@ -19,7 +22,11 @@ all_rslt$submethod[ind_SNN] <- paste0(
   all_rslt$cov_kernel[ind_SNN]
 )
 ind_VT <- which(all_rslt$method == "VT")
-all_rslt$submethod[ind_VT] <- "VMET[30]"
+all_rslt$submethod[ind_VT] <- paste0(
+  "VMET",
+  "[", all_rslt$m[ind_VT], "]_",
+  all_rslt$cov_kernel[ind_VT]
+)
 ind_TN <- which(all_rslt$method == "TN")
 all_rslt$submethod[ind_TN] <- "MET"
 ind_CB <- which(all_rslt$method == "CB")
@@ -29,39 +36,79 @@ m_vec <- sort(unique(m_cmp_rslt$m))
 m_length <- length(m_vec)
 
 # comparison between SNN and others ---------------------------------------
-score <- "CRPS"
-order <- NULL # "maximin" # scenario 1 and 2 do not have desc ordering results
+score_chosen <- "RMSE"
+order <- "maximin" # "maximin" # scenario 1 and 2 do not have desc ordering results
 if (is.null(order)) {
   mtd_vec <- c("CB", "SNN", "VT", "TN")
 } else {
   mtd_vec <- c("CB", paste0("SNN", "_order_", order), "VT", "TN")
 }
-for (scenario in sort(unique(m_cmp_rslt$scenario))) {
-  subset_mask <- all_rslt$score == score & all_rslt$scenario == scenario &
-    all_rslt$method %in% mtd_vec
-  subset_rslt <- all_rslt[subset_mask, , drop = FALSE]
-  subset_rslt$submethod <- factor(subset_rslt$submethod, levels = c(paste0(
-    "SNN",
-    "[", m_vec, "]_known"
-  ), "VMET[30]", "MET", "CSB", "SNN[30]_unknown"))
-  ggplot(data = subset_rslt, mapping = aes(x = submethod, y = value)) +
-    geom_boxplot(mapping = aes(fill = method), alpha = 0.5) +
-    scale_y_continuous(name = score) +
-    scale_x_discrete(labels = c(
-      expression(SNN[10]),
-      expression(SNN[20]),
-      expression(SNN[30]),
-      expression(SNN[40]),
-      expression(SNN[50]),
-      expression(VMET[30]),
-      expression(MET),
-      expression(CSB^"*"),
-      expression(SNN[30]^"*")
+for (scenario_chosen in sort(unique(m_cmp_rslt$scenario))) {
+  subset_score <- all_rslt %>%
+    filter(
+      score == score_chosen & scenario == scenario_chosen & method %in% mtd_vec
+    ) %>%
+    group_by(submethod, score) %>%
+    summarise(value_avg = mean(value))
+  subset_time <- all_rslt %>%
+    filter(
+      score == "time" & scenario == scenario_chosen & method %in% mtd_vec
+    ) %>%
+    group_by(submethod, score) %>%
+    summarise(value_avg = mean(value))
+  subset <- subset_score %>%
+    left_join(
+      subset_time,
+      by = "submethod"
+    ) %>%
+    rename(score = score.x, value = value_avg.x, time = value_avg.y) %>%
+    select(!score.y)
+
+  subset <- subset %>% mutate(submethod = ifelse(
+    grepl("SNN.*_known", submethod),
+    "SNN", submethod
+  ))
+  subset <- subset %>% mutate(submethod = ifelse(
+    grepl("SNN.*_unknown", submethod),
+    "SNN_unknown", submethod
+  ))
+  subset <- subset %>% mutate(submethod = ifelse(
+    grepl("VMET.*_known", submethod),
+    "VMET", submethod
+  ))
+  subset$submethod <- factor(subset$submethod, levels = c(
+    "SNN", "SNN_unknown", "VMET", "MET", "CSB"
+  ))
+
+  ggplot(data = subset, mapping = aes(x = time, y = value)) +
+    geom_point(mapping = aes(colour = submethod, shape = submethod), size = 3) +
+    geom_line(mapping = aes(group = submethod, colour = submethod)) +
+    scale_y_continuous(
+      name = score_chosen
+    ) +
+    scale_x_continuous(
+      name = "time (seconds)", trans = "log2"
+    ) +
+    scale_color_manual(labels = c(
+      expression("SNN"),
+      expression(SNN^"*"),
+      expression("VMET"),
+      expression("MET"),
+      expression(CSB^"*")
+    ), values = hue_pal()(5)) +
+    scale_shape_manual(values = 1:5, labels = c(
+      expression("SNN"),
+      expression(SNN^"*"),
+      expression("VMET"),
+      expression("MET"),
+      expression(CSB^"*")
     )) +
+    # ggtitle(paste("Scenario", prob_ind)) +
     theme(
-      text = element_text(size = 16), legend.position = "none",
-      axis.title.x = element_blank(),
-      axis.title.y = element_blank()
+      text = element_text(size = 16),
+      legend.title = element_blank(),
+      plot.title = element_text(hjust = 0.5),
+      legend.position = c(0.13, 0.8)
     )
   if (is.null(order)) {
     order_ID <- 0
@@ -74,7 +121,7 @@ for (scenario in sort(unique(m_cmp_rslt$scenario))) {
   }
   ggsave(
     paste0(
-      "plots/performance_plot_scene_", scenario, "_", score,
+      "plots/performance_plot_scene_", scenario_chosen, "_", score_chosen,
       "_order_", order_ID, ".pdf"
     ),
     width = 5.5,
@@ -83,11 +130,11 @@ for (scenario in sort(unique(m_cmp_rslt$scenario))) {
 }
 
 # comparison of different orderings using SNN --------------------------------
-score <- "CRPS"
+score_chosen <- "CRPS"
 mtd_vec <- c("SNN", "SNN_order_desc", "SNN_order_maximin")
-scenario <- 3
+scenario_chosen <- 3
 
-subset_mask <- all_rslt$score == score & all_rslt$scenario == scenario &
+subset_mask <- all_rslt$score == score_chosen & all_rslt$scenario == scenario_chosen &
   all_rslt$method %in% mtd_vec
 subset_rslt <- all_rslt[subset_mask, , drop = FALSE]
 subset_rslt$method <- factor(subset_rslt$method,
@@ -96,7 +143,7 @@ subset_rslt$method <- factor(subset_rslt$method,
 subset_rslt$m <- factor(subset_rslt$m, levels = sort(unique(subset_rslt$m)))
 ggplot(data = subset_rslt, mapping = aes(x = m, y = value)) +
   geom_boxplot(mapping = aes(fill = method), alpha = 0.5) +
-  scale_y_continuous(name = score) +
+  scale_y_continuous(name = score_chosen) +
   scale_x_discrete(labels = paste0("m = ", unique(subset_rslt$m))) +
   scale_fill_discrete(labels = c("default", "var-desc", "maximin")) +
   theme(
@@ -107,7 +154,7 @@ ggplot(data = subset_rslt, mapping = aes(x = m, y = value)) +
     axis.title.y = element_blank()
   )
 ggsave(
-  paste0("plots/order_compare_scene_", scenario, "_", score, ".pdf"),
+  paste0("plots/order_compare_scene_", scenario_chosen, "_", score_chosen, ".pdf"),
   width = 5,
   height = 5
 )
